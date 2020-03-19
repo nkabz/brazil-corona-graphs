6713305a2608bbaad9bc7505849031c5e3de7c29
Nikolas Abiusi <nikolasab@gmail.com>
2020-03-19 03:12:37 -0300

    mini footer added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header () {
     return (
-        <div className='p-8 text-center text-3xl text-blue-800 shadow-xs font-thin'>
+        <div className='p-8 w-full text-center text-3xl text-blue-800 shadow-xs font-thin'>
             Novo Coronavirus (Covid-19) - Brasil
         </div>
     )
--- src/pages/App.js
+++ src/pages/App.js
@@ -6,10 +6,18 @@ import Graphs from '../components/Graphs'
 
 function App() {
     return (
-        <div className="grid ">
+        <div className="grid min-h-screen" style={{placeItems: 'center'}}>
             <Header />
-            <div className="w-full p-2 md:w-10/12 grid grid-cols-1 mx-auto">
+            <div className="w-full p-2 md:w-10/12 grid grid-cols-1">
                 <Graphs />
+            </div>
+            <div className="my-16">
+                <p className='text-xs text-gray-600 opacity-75'>
+                    Dados coletados de <a className='text-blue-700' href="https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series">https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series</a>
+                </p>
+                <p className='text-xs font-semibold text-gray-800 text-center my-8'>
+                    #FiqueEmCasa
+                </p>
             </div>
         </div>
     );
