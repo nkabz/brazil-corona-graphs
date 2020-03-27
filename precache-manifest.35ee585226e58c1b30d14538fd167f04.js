57c4a3decec3ac0cc8aae02bb5c5370eb726eaca
Nikolas Abiusi <nikolasab@gmail.com>
2020-03-27 13:47:30 -0300

    Updates

--- precache-manifest.35ee585226e58c1b30d14538fd167f04.js
+++ precache-manifest.35ee585226e58c1b30d14538fd167f04.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a905df02f86f8a6b3ae7983d72fb2324",
+    "revision": "291919977e93a4a0a08c497ed29d8a26",
     "url": "/brazil-corona-graphs/index.html"
   },
   {
-    "revision": "e802482491e2b3ff9173",
+    "revision": "8dc044a230f74d62b8aa",
     "url": "/brazil-corona-graphs/static/css/main.c5e4fff7.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/brazil-corona-graphs/static/js/2.b82b725d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e802482491e2b3ff9173",
-    "url": "/brazil-corona-graphs/static/js/main.e8482168.chunk.js"
+    "revision": "8dc044a230f74d62b8aa",
+    "url": "/brazil-corona-graphs/static/js/main.5bdbd7a5.chunk.js"
   },
   {
     "revision": "83fa8002ec5d4b29de0d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/brazil-corona-graphs/precache-manifest.403f23f29d7c98af522ebf23aeccc205.js"
+  "/brazil-corona-graphs/precache-manifest.35ee585226e58c1b30d14538fd167f04.js"
 );
 
 self.addEventListener('message', (event) => {
