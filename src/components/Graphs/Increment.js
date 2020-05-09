22f5ca8a0f94d362295565797b40aca1cc50035c
Nikolas Abiusi <nikolasab@gmail.com>
2020-05-09 02:34:23 -0300

    changed bar label style to tooltip

--- src/components/Graphs/Increment.js
+++ src/components/Graphs/Increment.js
@@ -6,6 +6,7 @@ import {
     VictoryTheme,
     VictoryLabel,
     VictoryContainer,
+    VictoryTooltip,
 } from 'victory';
 
 import { incrementPlotGraph, incrementAverage } from '../../lib/utils';
@@ -20,7 +21,7 @@ export default function Increment({ width, mapData, type, graphColor }) {
                 theme={VictoryTheme.material}
                 width={width}
                 height={550}
-                domainPadding={width < 500 ? 10 : 50}
+                domainPadding={width < 652 ? 0 : 50}
                 animate={{
                     duration: 2000,
                     onLoad: { duration: 1000 }
@@ -45,14 +46,21 @@ export default function Increment({ width, mapData, type, graphColor }) {
                     data={incrementData}
                     labels={({ datum }) => Math.floor(datum.y)}
                     style={{
-                        labels: { fill: `rgba(${graphColor}, 1)`, fontSize: () => width < 500 ? 8 : 14 },
+                        labels: { fill: `rgba(${graphColor}, 1)`, fontSize: () => width < 652 ? 12 : 14 },
                         data: {
                             fill: `rgba(${graphColor}, 0.5)`,
                             stroke: `rgba(${graphColor}, 1)`,
                             strokeWidth: 1,
                         },
                     }}
-                    labelComponent={<VictoryLabel dy={width < 500 ? 4 : -15} dx={width < 500 ? 18 : 0} angle={width < 500 ? -90 : 0} />}
+                    labelComponent={
+                        <VictoryTooltip
+                            flyoutStyle={{
+                                stroke: '#d7d7d7',
+                                fill: 'white',
+                            }}
+                        />
+                    }
                 />
             </VictoryChart>
             <div className='text-center italic'>
